9d3ec0c753248049241ce24736d67138d6d2a480
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-15 14:36:28 +0200

    Refactored GenerateFromScenario

--- src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/plugins/pcm/GenerateOLCsFromScenario.java
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/plugins/pcm/GenerateOLCsFromScenario.java
@@ -6,7 +6,9 @@ import com.inubit.research.gui.WorkbenchConnectToServerDialog;
 import com.inubit.research.gui.plugins.WorkbenchPlugin;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.IEdge;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.INode;
+import de.uni_potsdam.hpi.bpt.bp2014.conversion.activity_centric.ActivityCentricProcessModel;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.converter.activity_centric.ActivityCentricToSynchronizedOLC;
+import de.uni_potsdam.hpi.bpt.bp2014.conversion.converter.pcm.ScenarioToSynchronizedOLC;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.olc.DataObjectState;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.olc.ObjectLifeCycle;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.olc.StateTransition;
@@ -36,7 +38,6 @@ import java.util.*;
 public class GenerateOLCsFromScenario extends WorkbenchPlugin {
     private final Workbench wb;
     private Collection<PCMFragment> fragments;
-    private Map<String, Collection<ObjectLifeCycle>> olcsPerDataClass;
     private Collection<ObjectLifeCycle> olcs;
     private Collection<String> fragmentIDs;
 
@@ -51,7 +52,6 @@ public class GenerateOLCsFromScenario extends WorkbenchPlugin {
             @Override
             public void actionPerformed(ActionEvent e) {
                 fragments = new HashSet<>();
-                olcsPerDataClass = new HashMap<>();
                 olcs = new HashSet<>();
                 fragmentIDs = new HashSet<>();
                 ProcessModel model = wb.getSelectedModel();
@@ -74,7 +74,6 @@ public class GenerateOLCsFromScenario extends WorkbenchPlugin {
                     ModelDirectory directory = server.getDirectory();
                     extractModelsFromDirectory(directory);
                     generateObjectLifeCycles();
-                    integrateStates();
                     for (ObjectLifeCycle objectLifeCycle : olcs) {
                         ProcessModel olc = new de.uni_potsdam.hpi.bpt.bp2014.jeditor.visualization.olc.ObjectLifeCycle();
                         Map<INode, de.uni_potsdam.hpi.bpt.bp2014.jeditor.visualization.olc.DataObjectState> processedNodes = new HashMap<>();
@@ -137,63 +136,6 @@ public class GenerateOLCsFromScenario extends WorkbenchPlugin {
         return menuItem;
     }
 
-    private void integrateStates() {
-        for (Map.Entry<String, Collection<ObjectLifeCycle>> olcsAndName : olcsPerDataClass.entrySet()) {
-            Map<DataObjectState, Collection<StateTransition>> successors = new HashMap<>();
-            Map<String, DataObjectState> states = new HashMap<>();
-            for (ObjectLifeCycle olc : olcsAndName.getValue()) {
-                for (IEdge transition : olc.getEdgeOfType(StateTransition.class)) {
-                    if (!((DataObjectState)transition.getSource()).getName().equals("i")) {
-                        String sourceName = ((DataObjectState) transition.getSource()).getName();
-                        String targetName = ((DataObjectState) transition.getTarget()).getName();
-                        if (!states.containsKey(sourceName)) {
-                            states.put(sourceName, new DataObjectState(sourceName));
-                        }
-                        if (!states.containsKey(targetName)) {
-                            states.put(targetName, new DataObjectState(targetName));
-                        }
-                        if (!successors.containsKey(states.get(sourceName))) {
-                            successors.put(states.get(sourceName), new HashSet<StateTransition>());
-                        }
-                        if (!successorExists(successors.get(states.get(sourceName)), (StateTransition) transition)) {
-                            StateTransition newTransition = new StateTransition(
-                                    states.get(sourceName),
-                                    states.get(targetName),
-                                    ((StateTransition) transition).getLabel());
-                            successors.get(states.get(sourceName)).add(newTransition);
-                            states.get(sourceName).addOutgoingEdge(newTransition);
-                            states.get(targetName).addIncomingEdge(newTransition);
-                        }
-                    }
-                }
-            }
-            ObjectLifeCycle olc = new ObjectLifeCycle(olcsAndName.getKey());
-            for (DataObjectState dataObjectState : states.values()) {
-                olc.addNode(dataObjectState);
-                if (dataObjectState.getName().equals("init")) {
-                    olc.setStartNode(dataObjectState);
-                }
-                if (dataObjectState.getOutgoingEdges().isEmpty()) {
-                    olc.addFinalNode(dataObjectState);
-                }
-            }
-            olcs.add(olc);
-        }
-    }
-
-    private boolean successorExists(Collection<StateTransition> stateTransitions, StateTransition transition) {
-        for (StateTransition stateTransition : stateTransitions) {
-            if (((DataObjectState)stateTransition.getSource()).getName().equals(
-                    ((DataObjectState)transition.getSource()).getName()) &&
-                    ((DataObjectState)stateTransition.getTarget()).getName().equals(
-                            ((DataObjectState)transition.getTarget()).getName()) &&
-                    stateTransition.getLabel().equals(transition.getLabel())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     private void extractModelsFromDirectory(ModelDirectory directory) throws Exception {
         for (ModelDirectoryEntry modelDirectoryEntry : directory.getEntries()) {
             if (modelDirectoryEntry instanceof ModelDirectory) {
@@ -208,16 +150,12 @@ public class GenerateOLCsFromScenario extends WorkbenchPlugin {
     }
 
     private void generateObjectLifeCycles() {
+        Collection<ActivityCentricProcessModel> adaptedFragments = new HashSet<>();
         for (PCMFragment fragment : fragments) {
-            ACPMAdapter modelAdapted = new ACPMAdapter(fragment);
-            ActivityCentricToSynchronizedOLC acpm2solc = new ActivityCentricToSynchronizedOLC();
-            SynchronizedObjectLifeCycle solc = (SynchronizedObjectLifeCycle) acpm2solc.convert(modelAdapted);
-            for (ObjectLifeCycle objectLifeCycle : solc.getOLCs()) {
-                if (!olcsPerDataClass.containsKey(objectLifeCycle.getLabel())) {
-                    olcsPerDataClass.put(objectLifeCycle.getLabel(), new HashSet<ObjectLifeCycle>());
-                }
-                olcsPerDataClass.get(objectLifeCycle.getLabel()).add(objectLifeCycle);
-            }
+            adaptedFragments.add(new ACPMAdapter(fragment));
         }
+        ScenarioToSynchronizedOLC converter = new ScenarioToSynchronizedOLC();
+        olcs = (converter.convert(adaptedFragments))
+                .getOLCs();
     }
 }
