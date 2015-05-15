4dbbcdfea55dc60e9af9a5e7f3e965734a55d54b
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-15 08:56:38 +0200

    Pseudo state 'i' is removed from olc after generation

--- src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/plugins/pcm/GenerateOLCsFromScenario.java
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/plugins/pcm/GenerateOLCsFromScenario.java
@@ -138,32 +138,34 @@ public class GenerateOLCsFromScenario extends WorkbenchPlugin {
             Map<String, DataObjectState> states = new HashMap<>();
             for (ObjectLifeCycle olc : olcsAndName.getValue()) {
                 for (IEdge transition : olc.getEdgeOfType(StateTransition.class)) {
-                    String sourceName = ((DataObjectState)transition.getSource()).getName();
-                    String targetName = ((DataObjectState)transition.getTarget()).getName();
-                    if (!states.containsKey(sourceName)) {
-                        states.put(sourceName, new DataObjectState(sourceName));
-                    }
-                    if (!states.containsKey(targetName)) {
-                        states.put(targetName, new DataObjectState(targetName));
-                    }
-                    if (!successors.containsKey(states.get(sourceName))) {
-                        successors.put(states.get(sourceName), new HashSet<StateTransition>());
-                    }
-                    if (!successorExists(successors.get(states.get(sourceName)), (StateTransition) transition)) {
-                        StateTransition newTransition = new StateTransition(
-                                states.get(sourceName),
-                                states.get(targetName),
-                                ((StateTransition) transition).getLabel());
-                        successors.get(states.get(sourceName)).add(newTransition);
-                        states.get(sourceName).addOutgoingEdge(newTransition);
-                        states.get(targetName).addIncomingEdge(newTransition);
+                    if (!((DataObjectState)transition.getSource()).getName().equals("i")) {
+                        String sourceName = ((DataObjectState) transition.getSource()).getName();
+                        String targetName = ((DataObjectState) transition.getTarget()).getName();
+                        if (!states.containsKey(sourceName)) {
+                            states.put(sourceName, new DataObjectState(sourceName));
+                        }
+                        if (!states.containsKey(targetName)) {
+                            states.put(targetName, new DataObjectState(targetName));
+                        }
+                        if (!successors.containsKey(states.get(sourceName))) {
+                            successors.put(states.get(sourceName), new HashSet<StateTransition>());
+                        }
+                        if (!successorExists(successors.get(states.get(sourceName)), (StateTransition) transition)) {
+                            StateTransition newTransition = new StateTransition(
+                                    states.get(sourceName),
+                                    states.get(targetName),
+                                    ((StateTransition) transition).getLabel());
+                            successors.get(states.get(sourceName)).add(newTransition);
+                            states.get(sourceName).addOutgoingEdge(newTransition);
+                            states.get(targetName).addIncomingEdge(newTransition);
+                        }
                     }
                 }
             }
             ObjectLifeCycle olc = new ObjectLifeCycle(olcsAndName.getKey());
             for (DataObjectState dataObjectState : states.values()) {
                 olc.addNode(dataObjectState);
-                if (dataObjectState.getName().equals("i")) {
+                if (dataObjectState.getName().equals("init")) {
                     olc.setStartNode(dataObjectState);
                 }
                 if (dataObjectState.getOutgoingEdges().isEmpty()) {
