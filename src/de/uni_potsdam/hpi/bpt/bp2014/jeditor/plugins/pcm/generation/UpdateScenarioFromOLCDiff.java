60c5d7f40e11a2e917d4edefb6b6bb42aec2fd7b
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-18 16:01:54 +0200

    Implemented Update Plugin

--- src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/plugins/pcm/generation/UpdateScenarioFromOLCDiff.java
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/plugins/pcm/generation/UpdateScenarioFromOLCDiff.java
@@ -8,16 +8,23 @@ import de.uni_potsdam.hpi.bpt.bp2014.conversion.IModel;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.converter.olc.FragmentsFromOLCVersions;
 import de.uni_potsdam.hpi.bpt.bp2014.jeditor.converter.adapter.olc.OLCAdapter;
 import de.uni_potsdam.hpi.bpt.bp2014.jeditor.visualization.olc.ObjectLifeCycle;
+import de.uni_potsdam.hpi.bpt.bp2014.jeditor.visualization.pcm.PCMFragment;
+import de.uni_potsdam.hpi.bpt.bp2014.jeditor.visualization.pcm.PCMFragmentNode;
+import de.uni_potsdam.hpi.bpt.bp2014.jeditor.visualization.pcm.PCMScenario;
+import net.frapu.code.visualization.ProcessEdge;
 import net.frapu.code.visualization.ProcessModel;
+import net.frapu.code.visualization.ProcessNode;
+import net.frapu.code.visualization.bpmn.Association;
+import net.frapu.code.visualization.bpmn.Task;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class UpdateScenarioFromOLCDiff extends GeneratePCMFragmentFromMultipleOLC {
     Map<ObjectLifeCycle, ObjectLifeCycle> predecessors;
     Map<OLCAdapter, OLCAdapter> wrappedPredecessors;
+    PCMScenario scenario;
+    private Collection<PCMFragment> fragments;
 
     public UpdateScenarioFromOLCDiff(Workbench wb) {
         super(wb);
@@ -25,11 +32,20 @@ public class UpdateScenarioFromOLCDiff extends GeneratePCMFragmentFromMultipleOL
 
     @Override
     protected String getName() {
-        return "Generate Fragments From OLC Versions";
+        return "Update Scenario From OLC Versions";
     }
 
     @Override
     protected Collection<? extends ProcessModel> extractModelsFromSubDirectory(ModelDirectory directory) {
+        if (!(wb.getSelectedModel() instanceof PCMScenario)) {
+            System.err.println("The model should be a PCMScenario");
+            return new ArrayList<>(0);
+        }
+        scenario = (PCMScenario) wb.getSelectedModel();
+        Collection<String> fragmentIds = new HashSet<>();
+        for (ProcessNode processNode : scenario.getNodes()) {
+            fragmentIds.add(processNode.getProperty(PCMFragmentNode.PROP_FRAGMENT_MID));
+        }
         for (ModelDirectoryEntry directoryEntry : directory.getEntries()) {
             if (directoryEntry instanceof ModelDirectory) {
                 extractModelsFromSubDirectory((ModelDirectory) directoryEntry);
@@ -37,10 +53,12 @@ public class UpdateScenarioFromOLCDiff extends GeneratePCMFragmentFromMultipleOL
                 try {
                     if (!((ModelDescription) directoryEntry).getHead().getPredecessors().isEmpty()) {
                         ProcessModel model = ((ModelDescription) directoryEntry).getHead().getProcessModel();
-                        ProcessModel predecessor = ((ModelDescription) directoryEntry).getPredecessors(
-                                ((ModelDescription) directoryEntry).getHead()).iterator().next().getProcessModel();
                         if (model instanceof ObjectLifeCycle) {
+                            ProcessModel predecessor = ((ModelDescription) directoryEntry).getPredecessors(
+                                    ((ModelDescription) directoryEntry).getHead()).iterator().next().getProcessModel();
                             predecessors.put((ObjectLifeCycle) model, (ObjectLifeCycle) predecessor);
+                        } else if (model instanceof PCMFragment && fragmentIds.contains(model.getId())) {
+                            fragments.add((PCMFragment) model);
                         }
                     }
                 } catch (Exception e) {
@@ -58,6 +76,32 @@ public class UpdateScenarioFromOLCDiff extends GeneratePCMFragmentFromMultipleOL
     }
 
     @Override
+    protected Collection<? extends ProcessModel> convertModels(Collection<? extends IModel> generatedModels) {
+        Collection<PCMFragment> smallFragments = (Collection<PCMFragment>) super.convertModels(generatedModels);
+        Collection<PCMFragment> newFragments = new HashSet<>(smallFragments);
+        for (ProcessModel smallFragment : smallFragments) {
+            for (ProcessNode processNode : smallFragment.getNodesByClass(Task.class)) {
+                for (PCMFragment fragment : fragments) {
+                    for (ProcessNode node : fragment.getNodeByName(processNode.getName())) {
+                        newFragments.remove(smallFragment);
+                        for (ProcessEdge processEdge : smallFragment.getIncomingEdges(Association.class, processNode)) {
+                            Association newEdge = new Association(processEdge.getSource(), node);
+                            fragment.addEdge(newEdge);
+                            fragment.addNode(processEdge.getSource());
+                        }
+                        for (ProcessEdge processEdge : smallFragment.getOutgoingEdges(Association.class, processNode)) {
+                            Association newEdge = new Association(node, processEdge.getTarget());
+                            fragment.addEdge(newEdge);
+                            fragment.addNode(processEdge.getSource());
+                        }
+                    }
+                }
+            }
+        }
+        return newFragments;
+    }
+
+    @Override
     protected Collection<? extends IModel> wrapModels(Collection<? extends ProcessModel> models) {
         for (ProcessModel olc : models) {
             wrappedPredecessors.put(new OLCAdapter((ObjectLifeCycle) olc),
@@ -72,5 +116,6 @@ public class UpdateScenarioFromOLCDiff extends GeneratePCMFragmentFromMultipleOL
         super.initialize();
         predecessors = new HashMap<>();
         wrappedPredecessors = new HashMap<>();
+        fragments = new HashSet<>();
     }
 }
