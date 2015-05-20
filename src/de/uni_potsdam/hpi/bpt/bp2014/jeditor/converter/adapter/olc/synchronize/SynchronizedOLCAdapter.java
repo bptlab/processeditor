2f7b034111cf7a272c31287afe4e9b121bc0d78e
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-20 22:17:01 +0200

    Added package-info to adapter.olc.synchronize package

--- src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/olc/synchronize/SynchronizedOLCAdapter.java
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/olc/synchronize/SynchronizedOLCAdapter.java
@@ -10,11 +10,24 @@ import de.uni_potsdam.hpi.bpt.bp2014.jeditor.converter.adapter.olc.OLCAdapter;
 import java.util.*;
 
 /**
- * This
+ * This Class represents an adapter for the synchronized object life cycles.
+ * A Synchronized Object Life cycles is based on a collection of Object Life Cycles.
+ * And on synchronization edges.
  */
 public class SynchronizedOLCAdapter extends SynchronizedObjectLifeCycle {
+    /**
+     * Holds the Collection of Object Life Cycles wrapped by this SynchronizedOLCAdapter.
+     * The aggregation of all this life cycles is the synchronized olc.
+     */
     Set<ObjectLifeCycle> olcs;
 
+    /**
+     * Creates a new synchronized object life cycle adapter based on a collection
+     * of Object Life Cycles.
+     * First every Object life cycle will be wrapped with an adapter.
+     * Afterwards the synchronization edges will be initialized.
+     * @param olcs The Object Life Cycles which represent the synchronized OLC.
+     */
     public SynchronizedOLCAdapter(Collection<de.uni_potsdam.hpi.bpt.bp2014.jeditor.visualization.olc.ObjectLifeCycle> olcs) {
         super();
         this.olcs = new HashSet<>();
@@ -26,6 +39,14 @@ public class SynchronizedOLCAdapter extends SynchronizedObjectLifeCycle {
         initSynchronizationEdges();
     }
 
+    /**
+     * This method initializes the synchronization edges.
+     * Therefore it extracts all state transitions from all the object life cycles.
+     * Afterwards those transitions will be grouped.
+     * Which means for every transition all the transitions referred with the synchronized
+     * edges property will determined and clustered.
+     * The result will be saved inside the synchronisationEdges map.
+     */
     private void initSynchronizationEdges() {
         List<StateTransition> transitions = new LinkedList<>();
         for (ObjectLifeCycle olc : olcs) {
