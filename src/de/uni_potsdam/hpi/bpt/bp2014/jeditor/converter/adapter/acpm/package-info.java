f7fb34151d119bfb64137cc3abb10ce97177fe38
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-20 21:21:14 +0200

    Added package-info.java ot converter.adapter.acpm package

---
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/acpm/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains adapters.
+ * Those adapters allow to create a Activity Centric Process Model needed for the transformations
+ * based on a given {@link net.frapu.code.visualization.bpmn.BPMNModel} or {@link de.uni_potsdam.hpi.bpt.bp2014.jeditor.visualization.pcm.PCMFragment}.
+ * For each node and edge exist one adapter.
+ */
+package de.uni_potsdam.hpi.bpt.bp2014.jeditor.converter.adapter.acpm;
