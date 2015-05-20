8bfaefe6438bbb8de10e19f508dbb777cc6f29d6
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-20 21:15:28 +0200

    Addded Java doc to GatewayAdapter

--- src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/acpm/GatewayAdapter.java
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/acpm/GatewayAdapter.java
@@ -4,11 +4,21 @@ import de.uni_potsdam.hpi.bpt.bp2014.conversion.activity_centric.Gateway;
 import net.frapu.code.visualization.ProcessNode;
 
 /**
- * Created by Stpehan on 07.05.2015.
+ * This is an Adapter for a Gateway.
+ * A Gateway can either be a {@link net.frapu.code.visualization.bpmn.ExclusiveGateway} or
+ * a {@link net.frapu.code.visualization.bpmn.ParallelGateway}.
+ * This type will be set from the outside.
  */
 public class GatewayAdapter extends Gateway {
+    /**
+     * The gateway wrapped by this adapter.
+     */
     private net.frapu.code.visualization.bpmn.Gateway gateway;
 
+    /**
+     * Creates a new instance.
+     * @param processNode The Gateway node to be wrapped.
+     */
     public GatewayAdapter(net.frapu.code.visualization.bpmn.Gateway processNode) {
         gateway = processNode;
     }
