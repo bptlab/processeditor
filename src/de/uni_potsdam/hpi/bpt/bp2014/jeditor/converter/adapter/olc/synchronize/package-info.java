971f9a0c0addc5ae8fc27f78719e07b628aeb169
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-20 22:29:59 +0200

    Added package-info to plugins.pcm package

--- src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/olc/synchronize/package-info.java
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/olc/synchronize/package-info.java
@@ -1,6 +1,6 @@
 /**
  * This package contains all the classes necessary to adapt a synchronized Object Life Cycle.
  * A Synchronized Object life cycle is currently modeled implicitly by a collection of
- * Ojbect Life Cycles.
+ * Object Life Cycles.
  */
 package de.uni_potsdam.hpi.bpt.bp2014.jeditor.converter.adapter.olc.synchronize;
---
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/plugins/pcm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package old all Workbench plugins used for the PCM Models.
+ */
+package de.uni_potsdam.hpi.bpt.bp2014.jeditor.plugins.pcm;
