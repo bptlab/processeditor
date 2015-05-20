820afc01dbeaf8f4d995e1ccb083fd1649c35101
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-20 21:10:49 +0200

    Added java doc to event adapter

--- src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/acpm/EventAdapter.java
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/converter/adapter/acpm/EventAdapter.java
@@ -4,11 +4,20 @@ import de.uni_potsdam.hpi.bpt.bp2014.conversion.activity_centric.Event;
 import net.frapu.code.visualization.ProcessNode;
 
 /**
- * Created by Stpehan on 07.05.2015.
+ * This class is a wrapper for an Event it can either
+ * be an Start or an End event.
  */
 public class EventAdapter extends Event {
+    /**
+     * Holds the event wrapped by this adapter.
+     */
     private net.frapu.code.visualization.bpmn.Event event;
 
+    /**
+     * Creates a new adapter for a given Event
+     * the edges and Type have to be set from the outside.
+     * @param processNode The node which will be wrapped by this adapter.
+     */
     public EventAdapter(net.frapu.code.visualization.bpmn.Event processNode) {
         event = processNode;
     }
