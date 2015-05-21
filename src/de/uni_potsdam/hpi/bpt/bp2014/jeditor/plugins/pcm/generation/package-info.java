61d700415d434aa40b290f88989513c06adb2d7e
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-21 07:42:02 +0200

    Added package info to plugins.pcm.generation

---
+++ src/de/uni_potsdam/hpi/bpt/bp2014/jeditor/plugins/pcm/generation/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains classes representing workbench plugins.
+ * All plugins trigger a converter which allows us to generate
+ * a new process model.
+ * Depending on which plugin you execute,
+ * either the original or the generated model must be a PCMScenario/Fragment.
+ */
+package de.uni_potsdam.hpi.bpt.bp2014.jeditor.plugins.pcm.generation;
